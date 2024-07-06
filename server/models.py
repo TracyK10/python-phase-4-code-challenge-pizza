5a0c1ef7de1e19f30a94c22694e4472a9115417c
Tracy Karanja <chirutracy@gmail.com>
2024-07-06 13:49:56 +0300

    changed modely.py

--- server/models.py
+++ server/models.py
@@ -21,14 +21,10 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-    pizzas = db.relationship(
-        "Pizza",
-        secondary="restaurant_pizzas",
-        back_populates="restaurants"
-    )
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates= 'restaurant')
 
     # add serialization rules
-    serialize_rules = ('-restaurant_pizzas', 'pizzas.restaurants',)
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -42,14 +38,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-    restaurants = db.relationship(
-        "Restaurant",
-        secondary="restaurant_pizzas",
-        back_populates="pizzas"
-    )
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates = 'pizza')
 
     # add serialization rules
-    serialize_rules = ('-restaurant_pizzas', 'restaurants.pizzas',)
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -65,8 +57,8 @@ class RestaurantPizza(db.Model, SerializerMixin):
     restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable=False)
     pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"), nullable=False)
     
-    restaurant = db.relationship("Restaurant", backref=db.backref("restaurant_pizzas", cascade="all, delete-orphan"))
-    pizza = db.relationship("Pizza", backref=db.backref("restaurant_pizzas", cascade="all, delete-orphan"))
+    restaurant = db.relationship("Restaurant", back_populates = 'restaurant_pizzas')
+    pizza = db.relationship("Pizza", back_populates = 'restaurant_pizzas')
 
     # add serialization rules
     serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas',)
